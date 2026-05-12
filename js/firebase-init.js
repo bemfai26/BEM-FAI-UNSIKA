c8ccf92c35d8226a793438b2168457d300bb2e0c
bemfai26 <departemenkearsipan@gmail.com>
2026-05-13 00:58:53 +0700

    Update firebase-init.js

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -1,19 +1,19 @@
-// js/firebase-init.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
-import { getFirestore, collection, getDocs, query, orderBy, limit, doc, getDoc } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+import { getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import { getFirestore, collection, addDoc, updateDoc, deleteDoc, doc, getDoc, getDocs, query, orderBy, limit, where, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBjawHaidnv5SuDkDZTpZqtIVohltmRLzc",
-  authDomain: "gen-lang-client-0046328106.firebaseapp.com",
-  projectId: "gen-lang-client-0046328106",
-  storageBucket: "gen-lang-client-0046328106.firebasestorage.app",
-  messagingSenderId: "714430559081",
-  appId: "1:714430559081:web:551c94a1a01daf152943f8"
+  apiKey: "AIzaSyAz6l2XhAucqfgsLI8f1RgYnjpz63MFdTo",
+  authDomain: "bem-fai-unsika.firebaseapp.com",
+  projectId: "bem-fai-unsika",
+  storageBucket: "bem-fai-unsika.firebasestorage.app",
+  messagingSenderId: "218857328321",
+  appId: "1:218857328321:web:40f5a943367d3fd8080e40",
+  measurementId: "G-VTC20PTM5V"
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-export { auth, db, collection, getDocs, query, orderBy, limit, doc, getDoc };
+export { auth, db, collection, addDoc, updateDoc, deleteDoc, doc, getDoc, getDocs, query, orderBy, limit, where, serverTimestamp, onAuthStateChanged, signInWithEmailAndPassword, signOut };
